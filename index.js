4d9ef55f64ab99d603dddd1c308db9675138277b
Kim-hyeoksu <hyuksu0308@naver.com>
2021-07-10 19:24:02 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+
+// const { MongoClient } = require('mongodb');
+// const uri = "mongodb+srv://hsKim:hsKim@kim.dalle.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://hsKim:hsKim@kim.dalle.mongodb.net/kim?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+}).then(() => console.log('Mongo DB Connected...'))
+  .catch(err => console.log(err))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on http://localhost:${port}`)
+})
+
